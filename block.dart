e712c3f0588e6f4bfb1765579b91f9f847c8a866
Millenniar Studios <121758336+MillenniarSt@users.noreply.github.com>
2024-07-09 20:33:54 +0200

    Update block.dart

--- block.dart
+++ block.dart
@@ -1,11 +1,40 @@
-class BlockState implements JsonMappable<Map<String, dynamic>> {
+class BlockState implements JsonReaddable<Map<String, dynamic>> {
 
   late Map<Conditions, BlockModel> models;
+
+  BlockState(this.conditions);
+
+  BlockState.json(Map<String, dynamic> json) {
+    this.json(json);
+  }
+
+  void json(Map<String, dynamic> json) {
+    models = {
+      for(String conditions in json["variants"]?.keys)
+        Conditions.json(condition): BlockModel.json(json["variants"]![conditions]!)
+    }
+  }
 }
 
 class Conditions implements JsonMappable<String> {
 
-  late Map<String, String> conditions;
+  late final Map<String, String> conditions;
 
-  
+  Conditions(this.conditions);
+
+  Conditions.json(String json) {
+    this.json(json);
+  }
+
+  void json(String json) {
+    conditions = {
+      for(String conditions in conditions.split(","))
+        condition.substring(0, condition.indexOf("=")): condition.substring(condition.indexOf("=") +1)
+    }
+  }
+
+  String toJson() => [
+    for(String condition in conditions)
+      "$condition=${conditions[condition]!}"
+  ].join(",");
 }

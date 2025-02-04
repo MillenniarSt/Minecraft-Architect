346a55834ab9b7dc074f22e94456f8c32aa50277
Angelo <angelogaspa25@gmail.com>
2025-02-04 20:41:33 +0100

    Geo Update

--- src/world/geo/object.ts
+++ src/world/geo/object.ts
@@ -1,7 +1,6 @@
 import { Material } from "../../materials/material.js";
 import { Schematic } from "../../minecraft/schematic.js";
 import { Seed } from "../../util/random.js";
-import { Quaternion } from "../quaternion.js";
 import { Ray } from "../ray.js";
 import { Vec3 } from "../vector.js";
 
@@ -9,14 +8,11 @@ export class Object3 {
 
     constructor(
         public vertices: Vec3[],
-        public triangles: number[][],
-        public pos: Vec3,
-        public rotation: Quaternion = Quaternion.NORTH,
-        public scale: Vec3 = Vec3.UNIT
+        public triangles: number[][]
     ) { }
 
     static fromJson(json: any): Object3 {
-        return new Object3(json.vertices.map(Vec3.fromJson), json.triangles, Vec3.fromJson(json.pos), Quaternion.fromJson(json.rotation), Vec3.fromJson(json.scale))
+        return new Object3(json.vertices.map(Vec3.fromJson), json.triangles)
     }
 
     buildMaterial(material: Material, seed: Seed): Schematic {
@@ -62,10 +58,7 @@ export class Object3 {
     toJson() {
         return {
             vertices: this.vertices.map(v => v.toJson()),
-            triangles: this.triangles,
-            pos: this.pos,
-            rotation: this.rotation,
-            scale: this.scale
+            triangles: this.triangles
         }
     }
 }
